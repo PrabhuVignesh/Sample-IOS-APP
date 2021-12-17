dc4858ae87adb243adb37a761d4ff96899630ee3
Prabhu Vignesh Kumar Rajagopal <prabhuvigneshkumar.rajagopal@sprint.com>
2021-12-17 23:36:20 +0530

    Initial Commit

---
+++ IBM-POC-APP/IBM_POC_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBM_POC_APPApp.swift
+//  IBM-POC-APP
+//
+//  Created by Prabhu Vignesh Kumar on 17/12/21.
+//
+
+import SwiftUI
+
+@main
+struct IBM_POC_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
